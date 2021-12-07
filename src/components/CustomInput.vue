81eca2b0bb9555117719db8ea01306662c507e29
licc <lichangchou_lcc@mail.qzkeji.cn>
2021-12-07 17:08:57 +0800

    添加自定义v-model的演示代码

---
+++ src/components/CustomInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <input type="text" placeholder="请输入" :value="modelValue" @input="handleChange" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  methods: {
+    handleChange(event: any) {
+      this.$emit('update:modelValue', event.target.value)
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/playground/组件封装/自定义v-model.vue
@@ -0,0 +1,27 @@
+<template>
+  <p>vue的v-model其实是个语法糖</p>
+  <p>对自定义组件而言其底层是modelValue和update:modelValue事件的结合</p>
+  <p>&lt;comp v-model="param" /&gt;相当于&lt;comp :model-value="param" @update:model-value="param=$event"&gt;</p>
+  <p>comp组件中需要声明modelValue属性，并在适当的时候触发update:modelValue事件并给事件传参</p>
+  <custom-input v-model="customText"></custom-input><span>输入内容为：{{customText}}</span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import CustomInput from '@/components/CustomInput.vue'
+
+export default defineComponent({
+  components: { CustomInput },
+  data() {
+    return {
+      customText: ''
+    }
+  },
+  methods: {
+
+  }
+})
+</script>
+
+<style lang="scss">
+</style>

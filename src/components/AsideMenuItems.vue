a619b3b23107b375a4a9f6a2dc1c2a61863f8751
licc <lichangchou_lcc@mail.qzkeji.cn>
2021-12-20 17:17:03 +0800

    精简代码

--- src/components/AsideMenuItems.vue
+++ src/components/AsideMenuItems.vue
@@ -1,33 +1,32 @@
 <template>
   <template v-for="menu of menus" :key="menu.key">
-    <el-menu-item v-if="menu.isLeaf" :index="menu.key" @click="handleClick(menu)">{{ menu.label }}</el-menu-item>
+    <el-menu-item
+      v-if="menu.isLeaf"
+      :index="menu.key"
+      @click="handleClick(menu)"
+      >{{ menu.label }}</el-menu-item
+    >
     <el-sub-menu v-else :index="menu.key">
       <template #title>{{ menu.label }}</template>
-      <aside-menu-items :menus="menu.children" @item-click="handleClick"></aside-menu-items>
+      <aside-menu-items
+        :menus="menu.children"
+        @item-click="handleClick"
+      ></aside-menu-items>
     </el-sub-menu>
   </template>
 </template>
 
-<script lang="ts">
-import MenuItem from '@/types/menu-item'
-import Menus from '@/types/menus'
-import { defineComponent } from 'vue'
+<script lang="ts" setup>
+import MenuItem from "@/types/menu-item";
+import Menus from "@/types/menus";
 
-export default defineComponent({
-  props: {
-    menus: {
-      type: Menus,
-      default: () => []
-    }
-  },
-  emits: ['item-click'],
-  methods: {
-    handleClick(menu: MenuItem) {
-      this.$emit('item-click', menu)
-    }
-  }
-})
+defineProps<{
+  menus: Menus;
+}>();
+
+const emit = defineEmits(["item-click"]);
+
+function handleClick(menu: MenuItem) {
+  emit("item-click", menu);
+}
 </script>
-
-<style lang="scss">
-</style>

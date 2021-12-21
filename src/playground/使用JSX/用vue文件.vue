a03a8c6585f34d3ffc947b1fb8512896dff79f94
licc <lichangchou_lcc@mail.qzkeji.cn>
2021-12-21 10:28:11 +0800

    添加JSX特性演示

---
+++ src/playground/使用JSX/用vue文件.vue
@@ -0,0 +1,14 @@
+<script lang="tsx">
+import { ElCard } from "element-plus";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  render() {
+    return (
+      <ElCard>
+        将Vue单文件组件的script部分lang声明为tsx，就可以使用jsx语法
+      </ElCard>
+    );
+  },
+});
+</script>

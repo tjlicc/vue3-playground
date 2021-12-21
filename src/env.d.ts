d35865d560a948839afbf9fba9adf481c247c658
licc <lichangchou_lcc@mail.qzkeji.cn>
2021-12-21 20:30:28 +0800

    修改模块声明

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,15 @@
 /// <reference types="vite/client" />
+import { DefineComponent } from 'vue'
 
 declare module '*.vue' {
-  import { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+// 通过类型声明，可以让VSCode中的TS模块支持自定属性
+declare module '@vue/runtime-core' {
+  export interface ComponentCustomProperties {
+    $translate: (key: string) => any
+  }
 }
